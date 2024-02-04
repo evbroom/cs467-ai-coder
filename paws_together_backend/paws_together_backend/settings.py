48ac9deed5955c9666f615551a4a68248c5ab75e
Evan Broom <evanbroom@gmail.com>
2024-02-03 23:56:05 -0500

    added workflow to run tests.py in github actions

--- paws_together_backend/paws_together_backend/settings.py
+++ paws_together_backend/paws_together_backend/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -85,6 +86,19 @@ DATABASES = {
         'PORT': '5432',
     }
 }
+
+# Use the GitHub Actions database if the workflow is running
+if os.environ.get('GITHUB_WORKFLOW'):
+    DATABASES = {
+        'default': {
+           'ENGINE': 'django.db.backends.postgresql',
+           'NAME': 'github_actions',
+           'USER': 'postgres',
+           'PASSWORD': 'postgres',
+           'HOST': '127.0.0.1',
+           'PORT': '5432',
+        }
+    }
 
 
 # Password validation
