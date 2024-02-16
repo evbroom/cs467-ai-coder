a01918717410fd2eb2f0437dc2f2d3577549d126
Evan Broom <evanbroom@gmail.com>
2024-02-16 16:59:44 -0500

    adding option to use local or aws storage

--- paws_together_backend/paws_together_backend/settings.py
+++ paws_together_backend/paws_together_backend/settings.py
@@ -144,7 +144,11 @@ AWS_LOCATION = 'static'
 STATIC_URL = f'https://{AWS_S3_ENDPOINT_URL}/{AWS_LOCATION}/'
 # STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
-DEFAULT_FILE_STORAGE = os.getenv('DEFAULT_FILE_STORAGE')
+if 'AWS_STORAGE_BUCKET_NAME' in os.environ:
+    DEFAULT_FILE_STORAGE = os.getenv('DEFAULT_FILE_STORAGE')
+else:
+    DEFAULT_FILE_STORAGE = 'django.core.files.storage.FileSystemStorage'
+
 
 
 
