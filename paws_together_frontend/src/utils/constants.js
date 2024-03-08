6865aed17d58526dbbd054d53f2cbfbc26f69e69
Evan Broom <evanbroom@gmail.com>
2024-03-08 10:36:19 -0800

    updating react db and adding build.sh for render deployment

--- paws_together_frontend/src/utils/constants.js
+++ paws_together_frontend/src/utils/constants.js
@@ -1,2 +1,2 @@
-export const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:8000';
+export const API_URL = process.env.REACT_APP_API_URL || 'https://cs467-ai-coder-backend.onrender.com/';
 export const TOKEN_PREFIX = 'Token';
