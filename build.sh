e981c512a98918f3d8ffdecb8ef2203a6eb95663
Evan Broom <evanbroom@gmail.com>
2024-03-08 10:49:14 -0800

    moving requirements.txt install to build.sh file

--- build.sh
+++ build.sh
@@ -1,4 +1,12 @@
 #!/usr/bin/env bash
+# Exit on error
+set -o errexit
+
+# Modify this line as needed for your package manager (pip, poetry, etc.)
+pip install -r requirements.txt
+
+# Apply any outstanding database migrations
+python manage.py migrate
 
 if [[ $CREATE_SUPERUSER ]];
 then
